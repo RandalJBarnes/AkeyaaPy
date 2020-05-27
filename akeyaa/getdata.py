64a4b9e91b5fccc167eb24b2840c99e4800c6781
Randal Barnes <barne003@umn.edu>
2020-05-27 06:59:06 -0500

    Added get_state_polygon.

--- akeyaa/getdata.py
+++ akeyaa/getdata.py
@@ -4,12 +4,16 @@ A set of functions to retrieve data from specific ArgGIS Pro .gdb files.
 Functions
 ---------
 
------ get data -----
+----- get well data -----
 get_well_data(aquifers)
     Return well data from across Minnesota.
 
 get_well_data_by_polygon(polygon, aquifers)
     Return well data from across the polygon.
+
+----- state -----
+get_state_polygon()
+    Get the Minnesota state boundary polygon.
 
 ----- county -----
 get_county_code(cty_abbr)
@@ -24,7 +28,7 @@ get_county_name(cty_abbr)
 get_county_polygon(cty_abbr)
     Get the county polygon from the county abbreviation.
 
------ township, range, section -----
+----- township and section -----
 get_township_polygon(twnshp, rng)
     Get the township polygon.
 
@@ -54,6 +58,7 @@ get_subregion_polygon(subr_code)
 Notes
 -----
 o   Make sure that the localization file is present, correct, and complete.
+o   Our working coordinates are in 'NAD 83 UTM 15N'(EPSG:26915).
 
 Author
 ------
@@ -63,24 +68,25 @@ University of Minnesota
 
 Version
 -------
-26 May 2020
+27 May 2020
 
 """
 
 import pyproj
 import arcpy
 
-from localization import CTYLOC, CWILOC, TRSLOC, WBDLOC
+import localization as loc
 
 
 # Feature classes of interest.
-TWNRNG = TRSLOC + r'\tr'                        # township and range
-SECTIONS = TRSLOC + r'\trs'                     # township, range, and section
-COUNTIES = CTYLOC + r'\mn_county_boundaries'    # Minnesota counties
-WELLS = CWILOC + r'\allwells'                   # CWI allwells
-SWLS = CWILOC + r'\C5WL'                        # static water levels
-WTRSHD = WBDLOC + r'\WBDHU10'                   # hydrologic watersheds
-SUBREG = WBDLOC + r'\WBDHU8'                    # hydrologic sub-regions
+MNBDRY   = loc.STALOC + r'\Boundaries_of_Minnesota' # MN state boundary
+TWNBDRY  = loc.TRSLOC + r'\tr'                      # MN townships
+SECBDRY  = loc.TRSLOC + r'\trs'                     # MN sections
+CTYBDRY  = loc.CTYLOC + r'\mn_county_boundaries'    # MN counties
+ALLWELLS = loc.CWILOC + r'\allwells'                # MN county well index
+C5WL     = loc.CWILOC + r'\C5WL'                    # MN static water levels
+WBDHU10  = loc.WBDLOC + r'\WBDHU10'                 # US hydrologic watersheds
+WBDHU8   = loc.WBDLOC + r'\WBDHU8'                  # US hydrologic subregions
 
 # The attributes to be include in "well data".
 ATTRIBUTES = ['allwells.SHAPE',
@@ -137,6 +143,11 @@ def get_well_data(aquifers=None):
     welldata : list
         Each element in the list is a tuple containing all of the attributes
         included in the ATTRIBUTES constant (see above).
+
+    Notes
+    -----
+    o   Coordinates are in 'NAD 83 UTM 15N'(EPSG:26915).
+    o   This is a relatively slow (i.e. > 5 seconds) function call.
     """
 
     where = WHERE
@@ -153,8 +164,8 @@ def get_well_data(aquifers=None):
         else:
             raise ArgumentError
 
-    joined_table = arcpy.AddJoin_management(WELLS, 'RELATEID',
-                                            SWLS, 'RELATEID', False)
+    joined_table = arcpy.AddJoin_management(
+            ALLWELLS, 'RELATEID', C5WL, 'RELATEID', False)
 
     welldata = []
     with arcpy.da.SearchCursor(joined_table, ATTRIBUTES, where) as cursor:
@@ -188,6 +199,10 @@ def get_well_data_by_polygon(polygon, aquifers=None):
     welldata : list
         Each element in the list is a tuple containing all of the attributes
         included in the ATTRIBUTES constant (see above).
+
+    Notes
+    -----
+    o   Coordinates are in 'NAD 83 UTM 15N'(EPSG:26915).
     """
 
     where = WHERE
@@ -204,8 +219,8 @@ def get_well_data_by_polygon(polygon, aquifers=None):
         else:
             raise ArgumentError
 
-    joined_table = arcpy.AddJoin_management(WELLS, 'RELATEID',
-                                            SWLS, 'RELATEID', False)
+    joined_table = arcpy.AddJoin_management(
+            ALLWELLS, 'RELATEID', C5WL, 'RELATEID', False)
 
     located_wells = arcpy.SelectLayerByLocation_management(
         joined_table,
@@ -219,6 +234,33 @@ def get_well_data_by_polygon(polygon, aquifers=None):
             welldata.append(row)
 
     return welldata
+
+
+# -----------------------------------------------------------------------------
+def get_state_polygon():
+    """
+    Get the Minnesota state boundary polygon.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    polygon : arcpy.Polygon
+        https://pro.arcgis.com/en/pro-app/arcpy/classes/polygon.htm
+        The geographic focus of the query.
+
+    Notes
+    -----
+    o   Coordinates are in 'NAD 83 UTM 15N'(EPSG:26915).
+    """
+
+    with arcpy.da.SearchCursor(MNBDRY, ['SHAPE@']) as cursor:
+        try:
+            return cursor.next()[0]
+        except StopIteration:
+            raise NotFoundError
 
 
 # -----------------------------------------------------------------------------
@@ -260,7 +302,7 @@ def get_county_code(cty_abbr):
 
     where = "CTY_ABBR = '{0}'".format(cty_abbr)
 
-    with arcpy.da.SearchCursor(COUNTIES, ['COUN'], where) as cursor:
+    with arcpy.da.SearchCursor(CTYBDRY, ['COUN'], where) as cursor:
         try:
             return cursor.next()[0]
         except StopIteration:
@@ -284,11 +326,16 @@ def get_county_abbr(cty_code):
     cty_abbr : str
         The 4-character county abbreviation string, as defined by the
         Minnesota Department of Natural Resources.
+
+    Notes
+    -----
+    o   See get_county_code for details on the meaning and origin of the
+        county codes.
     """
 
     where = "COUN = {}".format(cty_code)
 
-    with arcpy.da.SearchCursor(COUNTIES, ['CTY_ABBR'], where) as cursor:
+    with arcpy.da.SearchCursor(CTYBDRY, ['CTY_ABBR'], where) as cursor:
         try:
             return cursor.next()[0]
         except StopIteration:
@@ -314,7 +361,7 @@ def get_county_name(cty_abbr):
 
     where = "CTY_ABBR = '{}'".format(cty_abbr)
 
-    with arcpy.da.SearchCursor(COUNTIES, ['CTY_NAME'], where) as cursor:
+    with arcpy.da.SearchCursor(CTYBDRY, ['CTY_NAME'], where) as cursor:
         try:
             return cursor.next()[0]
         except StopIteration:
@@ -341,7 +388,7 @@ def get_county_polygon(cty_abbr):
 
     where = "CTY_ABBR = '{}'".format(cty_abbr)
 
-    with arcpy.da.SearchCursor(COUNTIES, ['SHAPE@'], where) as cursor:
+    with arcpy.da.SearchCursor(CTYBDRY, ['SHAPE@'], where) as cursor:
         try:
             return cursor.next()[0]
         except StopIteration:
@@ -366,11 +413,15 @@ def get_township_polygon(twnshp, rng):
     polygon : arcpy.Polygon
         https://pro.arcgis.com/en/pro-app/arcpy/classes/polygon.htm
         The geographic focus of the query.
+
+    Notes
+    -----
+    o   Coordinates are in 'NAD 83 UTM 15N'(EPSG:26915).
     """
 
     where = "TOWN = {0:d} AND RANG = {1:d}".format(twnshp, rng)
 
-    with arcpy.da.SearchCursor(TWNRNG, ['SHAPE@'], where) as cursor:
+    with arcpy.da.SearchCursor(TWNBDRY, ['SHAPE@'], where) as cursor:
         try:
             return cursor.next()[0]
         except StopIteration:
@@ -398,12 +449,16 @@ def get_section_polygon(twnshp, rng, sctn):
     polygon : arcpy.Polygon
         https://pro.arcgis.com/en/pro-app/arcpy/classes/polygon.htm
         The geographic focus of the query.
+
+    Notes
+    -----
+    o   Coordinates are in 'NAD 83 UTM 15N'(EPSG:26915).
     """
 
     where = ("TOWN = {0:d} AND RANG = {1:d} AND SECT = {2:d}"
              .format(twnshp, rng, sctn))
 
-    with arcpy.da.SearchCursor(SECTIONS, ['SHAPE@'], where) as cursor:
+    with arcpy.da.SearchCursor(SECBDRY, ['SHAPE@'], where) as cursor:
         try:
             return cursor.next()[0]
         except StopIteration:
@@ -417,15 +472,15 @@ def get_watershed_code(wtrs_name):
 
     Parameters
     ----------
-    wtrshd : str
+    wtrs_name : str
         The watershed name string as defined in the U.S. Geological Survey
-        Watershed Boundary Dataset.
+        Watershed Boundary Dataset. These are not unique.
 
     Returns
     -------
     codes : list of tuples
         (code, state)
-        code : str
+        wtrs_code : str
             10-digit watershed number encoded as a string (HUC10).
         state : str
             Two character state abbreviations; e.g. 'MN'.
@@ -445,7 +500,7 @@ def get_watershed_code(wtrs_name):
     where = "(NAME = '{0}')".format(wtrs_name)
 
     codes = []
-    with arcpy.da.SearchCursor(WTRSHD, ['HUC10', 'STATES'], where) as cursor:
+    with arcpy.da.SearchCursor(WBDHU10, ['HUC10', 'STATES'], where) as cursor:
         try:
             for row in cursor:
                 codes.append(row)
@@ -467,14 +522,14 @@ def get_watershed_name(wtrs_code):
 
     Returns
     -------
-    wtrshd : str
+    WBDHU10 : str
         The watershed name string as defined in the U.S. Geological Survey
         Watershed Boundary Dataset. These are not unique.
     """
 
     where = "HUC10 = '{0}'".format(wtrs_code)
 
-    with arcpy.da.SearchCursor(WTRSHD, ['NAME'], where) as cursor:
+    with arcpy.da.SearchCursor(WBDHU10, ['NAME'], where) as cursor:
         try:
             return cursor.next()[0]
         except StopIteration:
@@ -509,7 +564,7 @@ def get_watershed_polygon(wtrs_code):
 
     where = "HUC10 = '{0}'".format(wtrs_code)
 
-    with arcpy.da.SearchCursor(WTRSHD, ['SHAPE@'], where) as cursor:
+    with arcpy.da.SearchCursor(WBDHU10, ['SHAPE@'], where) as cursor:
         try:
             poly7019 = cursor.next()[0]
         except StopIteration:
@@ -522,7 +577,7 @@ def get_watershed_polygon(wtrs_code):
 # -----------------------------------------------------------------------------
 def get_subregion_code(subr_name):
     """
-    Get the subregion code (HUC8) from the watershed name.
+    Get the subregion code(s) (HUC8) from the watershed name.
 
     Parameters
     ----------
@@ -532,21 +587,36 @@ def get_subregion_code(subr_name):
 
     Returns
     -------
-    subr_code : str
-        The 8-digit subregion number encoded as a string (HUC8).
+    codes : list of tuples
+        (code, state)
+        subr_code : str
+            The 8-digit subregion number encoded as a string (HUC8).
+        state : str
+            Two character state abbreviations; e.g. 'MN'.
+
+    Notes
+    -----
+    o   Subregion names are NOT unique. For example, there are two unique
+        "Snake" hydrologic subregions in Minnesota: one north of Thief River
+        Falls, and the other east of Brainerd.
+
+    References
+    ----------
+    https://www.usgs.gov/core-science-systems/ngp/national-hydrography/
+        access-national-hydrography-products
     """
 
     where = "NAME = '{0}'".format(subr_name)
 
-    code = []
-    with arcpy.da.SearchCursor(SUBREG, ['HUC8', 'STATES'], where) as cursor:
+    codes = []
+    with arcpy.da.SearchCursor(WBDHU8, ['HUC8', 'STATES'], where) as cursor:
         try:
             for row in cursor:
-                code.append(row)
+                codes.append(row)
         except StopIteration:
             raise NotFoundError
 
-    return code
+    return codes
 
 
 # -----------------------------------------------------------------------------
@@ -568,7 +638,7 @@ def get_subregion_name(subr_code):
 
     where = "HUC8 = '{0}'".format(subr_code)
 
-    with arcpy.da.SearchCursor(SUBREG, ['NAME'], where) as cursor:
+    with arcpy.da.SearchCursor(WBDHU8, ['NAME'], where) as cursor:
         try:
             return cursor.next()[0]
         except StopIteration:
@@ -603,7 +673,7 @@ def get_subregion_polygon(subr_code):
 
     where = "HUC8 = '{0}'".format(subr_code)
 
-    with arcpy.da.SearchCursor(SUBREG, ['SHAPE@'], where) as cursor:
+    with arcpy.da.SearchCursor(WBDHU8, ['SHAPE@'], where) as cursor:
         try:
             poly7019 = cursor.next()[0]
         except StopIteration:
@@ -660,7 +730,7 @@ def convert_polygon(poly7019):
     # Create the new arcpy.polygon.
     xy26915 = list(zip(x26915, y26915))
     array26915 = arcpy.Array([arcpy.Point(pt[0], pt[1]) for pt in xy26915])
-    poly26915 = arcpy.Geometry('polygon', array26915,
-                               arcpy.SpatialReference(26915))
+    poly26915 = arcpy.Geometry(
+            'polygon', array26915, arcpy.SpatialReference(26915))
 
     return poly26915
--- akeyaa/localization.py
+++ akeyaa/localization.py
@@ -9,24 +9,31 @@ University of Minnesota
 
 Version
 -------
-26 May 2020
+27 May 2020
 
 """
 
-# From the MN Department of Health.
+# Minnesota County Well Index (CWI). Obtained from the Minnesota Department
+# of Health. The CWI is administered by the Minnesota Geological Survey.
+# https://www.mngs.umn.edu/cwi.html
 CWILOC = r"D:\Google Drive\CWI\CWI_20200420\water_well_information.gdb"
 
-# Downloaded from:
+# Boundaries of Minnesota. Obtained from the Minnesota Geospatial Commons.
+# https://gisdata.mn.gov/dataset/bdry-state
+STALOC = r'D:\Google Drive\GIS\fgdb_bdry_state\bdry_state.gdb'
+
+# County Boundaries, Minnesota. Obtained from the Minnesota Geospatial Commons.
 # https://gisdata.mn.gov/dataset/bdry-counties-in-minnesota
 CTYLOC = r'D:\Google Drive\GIS\fgdb_bdry_counties_in_minnesota\bdry_counties_in_minnesota.gdb'
 
-# Downloaded from:
+# Township, Range, and Section (TRS) Boundaries Minnesota. Obtained from the
+# Minnesota Geospatial Commons.
 # https://gisdata.mn.gov/dataset/plan-mngeo-trs
 TRSLOC = r'D:\Google Drive\GIS\fgdb_plan_mngeo_trs\plan_mngeo_trs.gdb'
 
-# Downloaded from:
-# https://www.usgs.gov/core-science-systems/ngp/national-hydrography/access-national-hydrography-products
-# For more information see: https://pubs.usgs.gov/tm/11/a3/pdf/tm11-a3.pdf
+# National Hydrology Products: Watershed Boundary Dataset (WBD). Obtained
+# from the US Geological Survey
+# https://www.usgs.gov/core-science-systems/ngp/national-hydrography
 # Beware! This gdb uses lat/lon coordinates; more specifically 'GRS 1980'
 # (EPSG:7019), which is a precursor of 'WGS 84' (EPSG:4326).
 WBDLOC = r'D:\Google Drive\GIS\WBD_National_GDB\WBD_National_GDB.gdb'
